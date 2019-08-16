550d1af08747789fe9c74438380f9b58c41d5040
reganto <rreganto@gmail.com>
2019-08-16 13:32:49 +0430

    add docstring

--- vendor/utils/captcha/check.py
+++ vendor/utils/captcha/check.py
@@ -1,17 +1,20 @@
 import requests
-from vendor.keys.secret import CAPTCHA_SECRET_KEY
+from settings import settings
 
 # check captcha
 def check_captcha(g_recaptcha_response, remote_ip):
     """ check recaptcha function
     :parama g_recaptcha_response: response from recaptcha
-    (self.get_argument('g_recaptcha_response'))
+    (self.get_argument('g-recaptcha-response'))
     :param remote_ip: remote ip(request.remote_ip)
     :return: True or Flase
     """
 
-    query_string = '''?secret={0}&response={1}&remoteip={2}
-    '''.format(CAPTCHA_SECRET_KEY, g_recaptcha_response, remote_ip)
+    query_string = '?secret={0}&response={1}&remoteip={2}'.format(
+        settings.get('captcha_secret_key'),
+        g_recaptcha_response,
+        remote_ip
+    )
     api_call = "https://www.google.com/recaptcha/api/siteverify" + query_string
     api_response = requests.post(api_call)
     api_response = api_response.json()
