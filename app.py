1355dce9fa3537b259146a890e30804afda8adc3
reganto <rreganto@gmail.com>
2019-08-31 14:41:06 +0430

    add options.address to log line

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class Application(tornado.web.Application):
 
 
 def main():
-    print(f"Server (re)started on port {options.port}...")
+    print(f"Server (re)started on {options.address}:{options.port}")
     app = Application()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port, options.address)
