c25be2e49b568db44f47e50bc280815481361176
reganto <rreganto@gmail.com>
2019-10-28 18:41:45 +0330

    refine send emeil utility

--- utils/email/send.py
+++ utils/email/send.py
@@ -6,34 +6,27 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from settings import settings
 
-# send email
-def send_email(email, token):
-    """Send email function(SMTP)
+
+def send_email(email, text):
+    """Send email to users
     :param email: email recipient
-    :param token: token for verify user
+    :param text: email text
     :return: True or False
     """
+
     sender = settings.get('email_sender')
     recipient = email
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = recipient
     msg['Subject'] = 'Subject'
-    body = '''Click to below link to verify your email
-           <br />http://localhost:8888/register/checkmail
-           ?email={0}&token={1}'''.format(recipient, token)
 
-    msg.attach(MIMEText(body, 'html'))
+    msg.attach(MIMEText(text, 'html'))
 
-    # establish smtp server
     server = smtplib.SMTP('smtp.gmail.com', 587)
-    # start tls
     server.starttls()
-    # login
     server.login(sender, settings.get('email_password'))
-    # message
     text = msg.as_string()
-    # send mail
     response = server.sendmail(sender, recipient, text)
 
     if not response:
