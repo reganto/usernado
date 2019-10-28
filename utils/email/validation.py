4a5ac5ccf884b9ce7de3a7beec617f4a03d53de7
reganto <rreganto@gmail.com>
2019-10-28 18:52:34 +0330

    refine validation utility

--- utils/email/validation.py
+++ utils/email/validation.py
@@ -1,8 +1,11 @@
+# Get a secret code from https://neverbounce.com/
+# Change neverbounce_key in settings.py
+
 import requests
 from settings import settings
 
 
-def email_validation(this_email):
+def email_validation(email):
     """ validation received email
     :param email: received email
     :return: True or Flase
@@ -10,7 +13,7 @@ def email_validation(this_email):
 
     key = settings.get('neverbounce_key')
     payload = 'https://api.neverbounce.com/v4/single/check?key={0}&email={1}' \
-        .format(key, this_email)
+        .format(key, email)
     response = requests.post(payload)
     response = response.json()
     result = response.get('result')
