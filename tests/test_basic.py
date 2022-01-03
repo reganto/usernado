a4c3fc7be6e4a356e5ac234c13ca31bf3efce019
Reganto <rreganto@gmail.com>
2022-01-03 19:05:03 +0330

    tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,13 +3,13 @@ from app import Application
 
 
 class BasicTestCase(AsyncHTTPTestCase):
+
     def get_app(self):
         return Application()
 
     def test_homepage(self):
         response = self.fetch('/')
         self.assertEqual(response.code, 200)
-        self.assertIn(b'Home Page', response.body)
 
 
 if __name__ == '__main__':
