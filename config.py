97c49d5ce888a0f800e9b7007850060e3aab60d8
Reganto <rreganto@gmail.com>
2022-01-08 19:49:18 +0330

    I thought about a favicon so static_path added to config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent
 
 SETTINGS = dict(
         template_path=BASE_DIR / 'templates',
+        static_path=BASE_DIR / 'static',
         cookie_secret=os.urandom(16),
         )
 
