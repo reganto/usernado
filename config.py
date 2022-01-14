58467197fde2a05fdca48a11730cf2f6e8718f51
Reganto <rreganto@gmail.com>
2022-01-14 21:51:34 +0330

    Use secrets.token_ rather than os.urandom, I think it's more convenient

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+import secrets
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -6,6 +6,6 @@ BASE_DIR = Path(__file__).resolve().parent
 SETTINGS = dict(
         template_path=BASE_DIR / 'templates',
         static_path=BASE_DIR / 'static',
-        cookie_secret=os.urandom(16),
+        cookie_secret=secrets.token_bytes(),
         )
 
