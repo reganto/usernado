6c3f0514a012453373ba8f45a00bd07de26b02ef
Reganto <rreganto@gmail.com>
2022-01-19 21:29:10 +0330

    Add autoreload flag to detect changes in source files and  automatically restart server

--- config.py
+++ config.py
@@ -4,8 +4,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 SETTINGS = dict(
-        template_path=BASE_DIR / 'templates',
-        static_path=BASE_DIR / 'static',
-        cookie_secret=secrets.token_bytes(),
-        )
 
+        # Directory containing template files.
+        template_path=BASE_DIR / 'templates',
+
+        # Directory from which static files will be served.
+        static_path=BASE_DIR / 'static',
+
+        # Used by RequestHandler.get_secure_cookie and set_secure_cookie to sign cookies.
+        cookie_secret=secrets.token_bytes(),
+
+        # Automatically restart the server when a source file is modified. 
+        autoreload=True,
+        )
