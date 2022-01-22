43a3148ea6645d0cba10a572f38253f7efba3db2
Reganto <rreganto@gmail.com>
2022-01-22 19:16:16 +0330

    login test user

--- tests/base.py
+++ tests/base.py
@@ -3,7 +3,26 @@ from app import Application
 
 
 class BaseTestCase(AsyncHTTPSTestCase):
-
-
     def get_app(self) -> Application:
         return Application()
+
+    def login(self, url: str) -> dict:
+        """Login test user
+
+        :url: Login endpoint
+        :returns: dict
+        TODO: add username and password to logic
+
+        """
+        response = self.fetch(
+                url,
+                method="POST",
+                body=b"",
+                follow_redirects=False,
+                )
+        headers = {
+                'Cookie': response.headers['Set-Cookie'],
+                'Content-Type': 'application/x-www-form-urlencoded',
+                }
+       # users have to add new headers with this syntax -> headers.update()
+       return headers
