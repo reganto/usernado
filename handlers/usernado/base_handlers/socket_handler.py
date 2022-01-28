dcf4cae5247693b77bc9e3f2acc38ac70c514896
Reganto <rreganto@gmail.com>
2022-01-28 21:41:01 +0330

    Add send and broadcast methods to WebSocket handlers

--- handlers/usernado/base_handlers/socket_handler.py
+++ handlers/usernado/base_handlers/socket_handler.py
@@ -1,6 +1,37 @@
+from typing import Union, Any, Dict
+
+
 import tornado.websocket
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    pass
+    on_open = open
 
+    def send(
+            self,
+            message: Union[bytes, str, Dict[str, Any]],
+            binary: bool = False,
+            ):
+        """Send a message to specific participant
+
+        :message: Message to send
+        :returns: None
+
+        """
+        self.write_message(message, binary)
+
+    def broadcast(
+            self,
+            participants: set,
+            message: Union[bytes, str, Dict[str, Any]],
+            binary: bool = False,
+            ):
+        """Broadcast a message to all participants
+
+        :participants: Participants to send broadcast message
+        :message: Message to send to all participants
+        :returns: None
+
+        """
+        for participant in participants:
+            participant.send(message, binary)
