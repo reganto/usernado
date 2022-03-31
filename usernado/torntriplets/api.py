3fda5308a918bf2e78b503971f7c90ba983f0680
morgothonarda <102719808+morgothonarda@users.noreply.github.com>
2022-03-31 12:31:31 +0430

    api.py refactoring and documentation improvement

--- usernado/torntriplets/api.py
+++ usernado/torntriplets/api.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 import tornado.escape
 import tornado.web
 from usernado.torntriplets.base import BaseHandler
@@ -5,33 +7,27 @@ from usernado.torntriplets.base import BaseHandler
 
 class APIHandler(BaseHandler):
     def get_json_argument(self, name: str, default: str = None) -> str:
-        """Get json argument from current request
-
-        :param name: name of the argument
-        :type name: str
-        :param default: if name not provided then we use default, defaults to None
-        :type default: str, optional
-        :return: value of a json argument
-        :rtype: str
+        """Get json argument from current request.
+        :arg str name: Name of the argument.
+        :arg str default: Default value for argument if not presented.
+        .. versionadded:: 0.7.0
         """
         try:
             raw_data = self.request.body.decode().replace("'", '"')
         except Exception:
-            raise
+            raise ValueError("Data is malformed or is not provided.")
         else:
             json_data = tornado.escape.json_decode(raw_data)
             return json_data.get(name, default)
 
-    def get_json_arguments(self) -> dict:
-        """Get all json arguments from current request
-
-        :return: a dict of all json arguments
-        :rtype: dict
+    def get_json_arguments(self) -> Dict[Any, Any]:
+        """Get all json arguments from current request.
+        .. versionadded:: 0.7.0
         """
         try:
             raw_data = self.request.body.decode().replace("'", '"')
         except Exception:
-            raise
+            raise ValueError("Data is malformed or is not provided.")
         else:
             json_data = tornado.escape.json_decode(raw_data)
             return json_data
