2f77654f8c38bafd67f3a3a91a979e6cf1288a2b
Reganto <tell.reganto@gmail.com>
2022-04-14 21:33:51 +0430

    Create seperate errors module - refs #7

---
+++ usernado/errors.py
@@ -0,0 +1,7 @@
+from torntriplets.web import (
+    UserAlreadyExistError,
+    UserDoesNotExistError,
+    UnsupportedUserModelError,
+    PermissionDeniedError,
+)
+from torntriplets.api import DataMalformedOrNotProvidedError
