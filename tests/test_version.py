c3fdb2c4a52091a9379175aee87de2349b01925f
Reganto <tell.reganto@gmail.com>
2022-05-07 20:41:47 +0430

    version: change version to v0.2.4

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ from usernado import __version__
 
 class VersionTestCase(unittest.TestCase):
     def test_version(self):
-        assert __version__ == "0.2.3"
+        assert __version__ == "0.2.4"
